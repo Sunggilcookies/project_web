f690204a00caf1f1bddf1a8538224077e4f8ada5
Sunggilcookies <rabbitsg1234@naver.com>
2023-04-17 18:23:11 +0900

    dkjf

--- myprofile/js/main.js
+++ myprofile/js/main.js
@@ -18,7 +18,27 @@ function showPicture(){
     setTimeout(showPicture,1000); //콜백함수
 }
 
+ //ready(익명함수(){}) ,click(익명함수(){})
+ $(document).ready(function(){
+    //클릭 이벤트 - on함수
+    let sw=true;    //상태변수(전역변수)
+    $(".btn").on('click',function(){
 
+        if(sw){
+            $("nav").hide();
+            $(this).text("보임"); //이것을 숨김
+            
+            sw=false;
+        }
+        else{
+            $("nav").show();
+            $(this).text("보임"); //이것을 숨김
+            sw=true;
+        }
+
+      
+    });
+});
 
 
 
